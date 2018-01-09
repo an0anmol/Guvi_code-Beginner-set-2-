cf5564bc1103ce8234b09073f2729507a450bb9a
ano_anmol <anmolshukla7@gmail.com>
2018-01-10 00:06:28 +0530

    palindrome
    Check whether a number is palindrome or not.Print yes if its a palindrome no if its not

--- power.c
+++ power.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include<stdio.h>
 #include<math.h>
 void main()
@@ -6,4 +7,14 @@ int n,k,z;
 scanf("%d%d",&n,&k);
 z=pow(n,k);
 printf("%d",z);
+=======
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int n,k,z;
+scanf("%d%d",&n,&k);
+z=pow(n,k);
+printf("%d",z);
+>>>>>>> 1b4ebd3b26f7ff762f67e0966127d1dc62754639
 }
