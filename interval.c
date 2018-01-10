27e64c959b379214d3ba215a1199b35fa42943d5
ano_anmol <anmolshukla7@gmail.com>
2018-01-10 21:14:20 +0530

    print odd  number between two numbers
    Given 2 numbers(interval) Display(space separated) odd numbers between two intervals.

---
+++ interval.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int n,k,i,a[100],j=0;
+scanf("%d%d",&n,&k);
+for(i=n+1;i<k;i++)
+{
+if(i%2!=0)
+{
+a[j]=i;
+j++;
+}
+}
+for(i=0;i<j;i++)
+{
+printf("%d ",a[i]);
+}
+}
